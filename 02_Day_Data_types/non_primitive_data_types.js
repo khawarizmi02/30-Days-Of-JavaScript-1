adf3b17dd273176c3b984f653735484a5c9b7447
khawarizmi <khawarizmijefri02@gmail.com>
2023-01-25 15:11:29 +0800

    day 1 and day2 notes

--- 02_Day_Data_types/non_primitive_data_types.js
+++ 02_Day_Data_types/non_primitive_data_types.js
@@ -2,11 +2,11 @@ let nums = [1, 2, 3]
 nums[0] = 10
 console.log(nums) // [10, 2, 3]
 
-let nums = [1, 2, 3]
+let nums2 = [1, 2, 3]
 let numbers = [1, 2, 3]
 console.log(nums == numbers) // false
 
-let userOne = {
+let userOne1 = {
   name: 'Asabeneh',
   role: 'teaching',
   country: 'Finland'
@@ -16,15 +16,15 @@ let userTwo = {
   role: 'teaching',
   country: 'Finland'
 }
-console.log(userOne == userTwo) // false
+console.log(userOne1 == userTwo) // false
 
-let numbers = nums
-console.log(nums == numbers)  // true
+let numbers2 = nums
+console.log(nums == numbers2)  // true
 
 let userOne = {
 name:'Asabeneh',
 role:'teaching',
 country:'Finland'
 }
-let userTwo = userOne
-console.log(userOne == userTwo)  // true
+let userTwo2 = userOne
+console.log(userOne == userTwo2)  // true
